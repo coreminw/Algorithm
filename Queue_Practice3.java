6fce3d7d78baeef7b46f5ea173d30cbd11573841
김민욱 <coreminw@naver.com>
2022-02-24 12:03:39 +0900

    Queue의 기본함수

---
+++ Queue_Practice3.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_Practice3 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());//queue 값 빼내기
+        }
+    }
+}
