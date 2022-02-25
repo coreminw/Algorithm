38d8baf1c0e888f8f8f400305b08eeadef1df788
coreminw <coreminw@naver.com>
2022-02-25 16:44:46 +0900

    벡터와 반복자

---
+++ IteratorEx.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class IteratorEx {
+    public static void main(String[] args) {
+        Vector<Integer> v= new Vector<Integer>();
+        v.add(6);
+        v.add(4);
+        v.add(3);
+        v.add(2);
+        v.add(1,5);
+
+        Iterator<Integer> it = v.iterator();
+        while(it.hasNext()){
+            int n = it.next();
+            System.out.println(n);
+        }
+        
+    }
+}
