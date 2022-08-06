01336d661143f4d7f48105917bc1a7133a19a7a0
kimminwook <coreminw@naver.com>
2022-08-06 17:38:52 +0900

    백준 입출력예제 10953번

---
+++ Algorithm_10953.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Algorithm_10953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer stringTokenizer;
+        String str;
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+            str = sc.next();
+            stringTokenizer = new StringTokenizer(str, ",");
+
+            int A = Integer.parseInt(stringTokenizer.nextToken());
+            int B = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(A + B);
+
+        }
+    }
+}
