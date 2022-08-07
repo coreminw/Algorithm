3930402dc6d47a434b8c73b689f783de277d2991
kimminwook <coreminw@naver.com>
2022-08-07 19:36:31 +0900

    Algorithm 입출력

---
+++ Algorithm_11719.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Algorithm_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while((str = br.readLine()) != null){
+            System.out.println(str);
+        }
+    }
+}
---
+++ Algorithm_11720.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Algorithm_11720 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String n = sc.next();
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            sum += n.charAt(i) - 48;
+        }
+        System.out.println(sum);
+    }
+}
