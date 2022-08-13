46e17aedab1b59ff33604ac28f12e42d74faba75
kimminwook <coreminw@naver.com>
2022-08-13 23:20:15 +0900

    수 정렬하기1

---
+++ Algorithm_2751_수정렬하기2.java
@@ -0,0 +1,30 @@
+package 다양한문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm_2751_수정렬하기2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
