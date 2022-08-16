a587064f63a861ec31d92f1da8154a450bb4892a
kimminwook <coreminw@naver.com>
2022-08-17 00:54:58 +0900

    좌표정렬

---
+++ boj/다양한문제/Algorithm_11650_좌표정렬하기.java
@@ -0,0 +1,36 @@
+package boj.다양한문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Algorithm_11650_좌표정렬하기{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+                if(o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }else{
+                    return o1[0] - o2[0];
+                }
+        });
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
