fcdb59c40d81ada90a3f114535a538b4c90055bb
kimminwook <coreminw@naver.com>
2022-09-02 16:17:31 +0900

    불기 연도를 서기 연도로 변환

---
+++ boj/입출력/Algorithm_18108.java
@@ -0,0 +1,15 @@
+package boj.입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm_18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(y - (2541-1998));
+    }
+}
