865a4f28e86810cae34f1cdb1e999458680885ba
kimminwook <coreminw@naver.com>
2022-09-02 15:43:55 +0900

    물음표 출력

---
+++ boj/입출력/Algorithm_10926.java
@@ -0,0 +1,15 @@
+package boj.입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+
+        System.out.println(A+"??!");
+    }
+}
