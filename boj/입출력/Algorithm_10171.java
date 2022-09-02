5e7ec1a1fcd0761ab0992da6e9f79d184a7edc9b
kimminwook <coreminw@naver.com>
2022-09-02 17:05:14 +0900

    고양이 출력

---
+++ boj/입출력/Algorithm_10171.java
@@ -0,0 +1,10 @@
+package boj.입출력;
+
+public class Algorithm_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
