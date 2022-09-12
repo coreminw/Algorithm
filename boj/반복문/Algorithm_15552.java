71abd709ed0136a4d7ed32b506b3626a6f96b7ac
kimminwook <coreminw@naver.com>
2022-09-12 23:13:35 +0900

    빠른 A + B

--- boj/반복문/Algorithm_15552.java
+++ boj/반복문/Algorithm_15552.java
@@ -16,33 +16,7 @@ public class Algorithm_15552 {
             bw.write(A + B + "\n");
         }
 
-        bw.flush();package boj.반복문;
-
-import java.io.*;
-import java.util.StringTokenizer;
-
-        public class Algorithm_15552 {
-            private void solution() throws IOException{
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-                int T = Integer.parseInt(br.readLine());
-
-                for (int i = 0; i < T; i++) {
-                    StringTokenizer st = new StringTokenizer(br.readLine());
-                    int A = Integer.parseInt(st.nextToken());
-                    int B = Integer.parseInt(st.nextToken());
-                    bw.write(A + B + "\n");
-                }
-
-                bw.flush();
-                bw.close();
-            }
-
-            public static void main(String[] args) throws IOException{
-                new boj.반복문.Algorithm_15552().solution();
-            }
-        }
-
+        bw.flush();
         bw.close();
     }
 
